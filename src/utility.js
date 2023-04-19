c2eda4688dfd1db7e2216686bf22ff10f006d511
ydarissep <60859033+ydarissep@users.noreply.github.com>
2023-04-20 01:26:44 +0200

    Add files via upload

--- src/utility.js
+++ src/utility.js
@@ -109,6 +109,8 @@ function setDataList(){
 function getSpeciesSpriteSrc(speciesName){
     if(sprites[speciesName]){
         if(sprites[speciesName].length < 500){
+            localStorage.removeItem(speciesName)
+            spriteRemoveBgReturnBase64(speciesName, species)
             return species[speciesName]["sprite"]
         }
         else{
@@ -116,6 +118,7 @@ function getSpeciesSpriteSrc(speciesName){
         }
     }
     else{
+        spriteRemoveBgReturnBase64(speciesName, species)
         return species[speciesName]["sprite"]
     }
 }
