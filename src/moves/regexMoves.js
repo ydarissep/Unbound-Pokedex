e170e720060df178ffecceaff3c2f04b46972244
ydarissep <60859033+ydarissep@users.noreply.github.com>
2024-09-08 19:14:39 +0200

    Add files via upload

--- src/moves/regexMoves.js
+++ src/moves/regexMoves.js
@@ -36,7 +36,7 @@ function regexMovesDescription(textMovesDescription, moves){
 function regexMoves(textMoves, moves){
     const lines = textMoves.split("\n")
     let move = null, change = false
-    const regex = /ACTUAL_PLA_MOVE_POWERS|BUFFED_LEECH_LIFE|GEN_6_POWER_NERFS/
+    const regex = /ACTUAL_PLA_MOVE_POWERS|BUFFED_LEECH_LIFE|GEN_6_POWER_NERFS|DARK_VOID_ACC_NERF/
 
     lines.forEach(line => {
         const matchMoves = line.match(/\[ *(MOVE_\w+) *\]/i)
