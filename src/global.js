347f99536f36c3fa12b6983a469e366592ff0c63
ydarissep <60859033+ydarissep@users.noreply.github.com>
2026-05-12 03:13:29 +0200

    Update Credits

--- src/global.js
+++ src/global.js
@@ -22,7 +22,7 @@ fetch('https://raw.githubusercontent.com/ydarissep/dex-core/main/index.html').th
     await fetch("https://raw.githubusercontent.com/ydarissep/dex-core/main/src/global.js").then(async response => {
         return response.text()
     }).then(async text => {
-        text = text.replace("Credit to ris", "Credit to DMan16 for:\n- Enhancements settings\n\nCredit to ris")
+        text = text.replace("Credit to ris", "Credit to DMan16 for:\n- Randomized Abilities\n- Randomized Learnset\n- Rebalanced Stats\n\nCredit to Aussi for:\n- Randomized Species\n\nCredit to ris")
         await eval.call(window,text)
     }).catch(error => {
         console.warn(error)
